59171ca2326addb570ef5e44235bfd687e5931e4
Ján Sopko <jan.sopko@ringieraxelspringer.sk>
2020-10-31 10:06:47 +0100

    removed dead code

--- assets/js/react/components/create/cards/CardsWrapper.js
+++ assets/js/react/components/create/cards/CardsWrapper.js
@@ -1,14 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import Axios from "axios";
+import React from 'react';
 import CreateNewThemeCard from "./CreateNewThemeCard";
 import ContinueOnExistingThemesCard from "./ContinueOnExistingThemesCard";
 
 
 const CardsWrapper = () => {
-    const [hasThemes, setHasThemes] = useState(false);
-    useEffect(() => {
-
-    }, []);
     return(
         <div id="cards-wrapper">
             <CreateNewThemeCard/>
